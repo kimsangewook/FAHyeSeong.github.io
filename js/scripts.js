f4fa23850de73f8adcde360cd5d8c5c33ba600fb
q904q3 <q904q3@shinsegae.com>
2025-05-14 15:12:47 +0900

    수정된 소스 코드 반영

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,35 @@
+window.onload = function () {
+
+    // 무한 롤링을 위한 콘텐츠 복제
+    const banner = document.getElementById('banner');
+    banner.innerHTML += banner.innerHTML; // 콘텐츠 두 번 반복
+
+    // 지도 표시할 div 가져오기
+    const container = document.getElementById('map');
+
+    // 지도 옵션 설정
+    const options = {
+        center: new kakao.maps.LatLng(37.3364, 126.7210), // 경기도 시흥시 공단1대로 196번길 34 좌표
+        level: 4 // 확대 레벨
+    };
+
+    // 지도 생성
+    const map = new kakao.maps.Map(container, options);
+
+    // 마커 생성
+    const marker = new kakao.maps.Marker({
+        position: new kakao.maps.LatLng(37.3364, 126.7210) // 경기도 시흥시 공단1대로 196번길 34 좌표
+    });
+    marker.setMap(map);
+
+    // 주소에 대한 인포윈도우 생성
+    const infowindow = new kakao.maps.InfoWindow({
+        content: '<div style="padding:5px; width:260px;">경기도 시흥시 공단1대로 196번길 34<br/>정왕동, 시화공단2다 606호</div>'
+    });
+    infowindow.open(map, marker);
+};
+
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
